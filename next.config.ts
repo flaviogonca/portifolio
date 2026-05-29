c5955bfdf8fcae67f8c557ecad61a75bc9e67d1e
Flávio Gonçalves <flavioscobar09@gmail.com>
2026-05-29 15:09:38 +0100

    configure next export

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: process.cwd(),
-  /* config options here */
+  output: "export",
+
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   reactStrictMode: false,
 };
 
